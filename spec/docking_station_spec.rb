2970ad7e30f5e631433b6772bdb14c576ab22fe6
Salar Ghotaslo <Salar_@live.co.uk>
2021-01-05 17:33:51 +0000

    minor edits to rspec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,8 @@
-
-  require 'docking_station'
-
+#This looks for the file called docking_station
+  require 'docking_station' 
+#Describe is what we use to define a class?
   describe DockingStation do
+#Use expect for Method - might not need this bit yet but have added in
       it "should respond to release?" do
           expect(subject).to respond_to(:release?)
       end
