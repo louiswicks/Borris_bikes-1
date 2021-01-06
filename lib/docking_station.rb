58d004ba4a749bd6a3ed776077d4af34f5c0a946
bullhornfixie <dawson.jon@hotmail.com>
2021-01-06 11:31:42 +0000

    built rspec test

---
+++ lib/docking_station.rb
@@ -0,0 +1,10 @@
+def add_numbers
+  2 + 2
+end
+
+def release_bike
+  "release bikes"
+end
+    
+add_numbers
+release_bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,20 @@
-#This looks for the file called docking_station
-  require 'docking_station' 
-#Describe is what we use to define a class?
-  describe DockingStation do
-#Use expect for Method - might not need this bit yet but have added in
-      it "should respond to release?" do
-          expect(subject).to respond_to(:release?)
-      end
+require 'docking_station' 
+# Opens docking_station.rb which is saved in lib folder 
+
+# Example 1
+describe "testing a simple method in docking_station" do
+  it "adds 2 + 2" do
+    expect(add_numbers).to eq(4)
   end
+end
+
+# Example 2
+describe "testing a simple method in docking_station" do
+  it "outputs a message when called" do
+    expect(release_bike).to eq ("release bike")
+  end
+end
+
+# In terminal run 'rspec' whilst in project folder 'Borris_bikes-1'
+# Example 1 should pass
+# Example 2 should pass 
